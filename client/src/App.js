0473e47c253c54340a2618eacc16d38ed1443862
Jose Orozco <josearozco15@gmail.com>
2023-03-25 15:04:07 -0600

    /home,/signup,/login

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import SignUpPage from './components/SignUpPage';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/signup" component={SignUpPage} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ client/src/components/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Virtual Pet Website</h1>
+      <Link to="/signup">
+        <button>Adopt Me</button>
+      </Link>
+      <h1>Already have a pet?</h1>
+      <Link to="/login">
+        <button>Click Me</button>
+      </Link>
+    </div>
+  );
+}
+
+export default HomePage;
