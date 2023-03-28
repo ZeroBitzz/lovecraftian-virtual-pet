c51d521465ee1561ce9770c89bbaecd9174e2c90
Jose Orozco <josearozco15@gmail.com>
2023-03-27 19:21:06 -0600

    models

---
+++ server/models/Pet.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const petSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  hunger: {
+    type: Number,
+    required: true,
+    default: 50,
+  },
+  sleep: {
+    type: Number,
+    required: true,
+    default: 50,
+  },
+  cleanliness: {
+    type: Number,
+    required: true,
+    default: 50,
+  },
+  evolutionStage: {
+    type: String,
+    enum: ['Baby', 'Child', 'Adult'],
+    default: 'Baby',
+  },
+});
+
+const Pet = mongoose.model('Pet', petSchema);
+
+module.exports = Pet;
