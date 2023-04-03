5808ac73d16d59d82407a24921834a840ec68aec
Broderick <zerothesu@gmail.com>
2023-04-03 15:52:37 -0600

    add signup styling

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -3,20 +3,22 @@ import { Link } from "react-router-dom"
 
 function SignUp() {
     return (
-        <div>
+        <div className="signup-container">
             <Link to="/">
-                <button>go back</button>
+                <button>go back to login</button>
             </Link>
             <form
                 onSubmit={(e) => {
                     alert("you have signed up")
                     e.preventDefault()
                 }}
+                className="signup-form"
             >
                 <input type="text" placeholder="username" />
+                <input type="email" placeholder="email" />
                 <input type="password" placeholder="password" />
                 <input type="password" placeholder="repeat password" />
-                <button type="submit">sign up</button>
+                <button className="submit-signup-button">sign up</button>
             </form>
         </div>
     )
