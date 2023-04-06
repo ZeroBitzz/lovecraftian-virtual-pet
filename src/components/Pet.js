f6ba4c03cfe56bcc1156d027f9e3ebe4b5490a7a
Broderick <35615510+ZeroBitzz@users.noreply.github.com>
2023-04-06 14:14:11 -0600

    add loader for server fetch

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,16 +1,35 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 function Pet() {
+
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    }, []);
+
+    
     return (
         <div>
-            <container className="pet-container" >
-                <Link to="/">
-                  <button>logout</button>
-              </Link>
-                <img src="/pet-models/cthulhu/cthulhu-evo-3.svg" alt="cthulhu" className="pet-model"/>
-                <img src="/pet-models/cthulhu/cthulhu-background.svg" alt="cthulhu background" className="pet-background"/>
-            </container>
+            {loading ? (
+                <div className="loader-container">
+                    <div className="spinner"></div>
+                </div>
+              ) : (
+                <div>
+                    <container className="pet-container" >
+                        <Link to="/">
+                          <button>logout</button>
+                      </Link>
+                        <img src="/pet-models/cthulhu/cthulhu-evo-3.svg" alt="cthulhu" className="pet-model"/>
+                        <img src="/pet-models/cthulhu/cthulhu-background.svg" alt="cthulhu background" className="pet-background"/>
+                    </container>
+                </div>
+              )}
         </div>
     )
 }
